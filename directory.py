bef3cb6af29fbf50eef2c93858c64ae19010a140
SSN-1 <sandip.sn131@gmail.com>
2022-11-24 12:08:51 +0000

    Added banner image to directory

--- directory.py
+++ directory.py
@@ -9,7 +9,7 @@ import branca
 
 st.set_page_config(page_title="Sikh Business Directory")
 
-banner = Image.open('banner.png')
+banner = Image.open('banner.svg')
 st.image(banner)
 
 #st.title("Sikh Business Directory")
